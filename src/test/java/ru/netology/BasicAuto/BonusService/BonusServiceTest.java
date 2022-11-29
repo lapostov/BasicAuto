9c33793e1fdb0efdf1f378c9653f60ae1f157307
Александр Масляков <lapostov@yandex.ru>
2022-11-29 16:28:58 +0400

    Added Class HackService

--- src/test/java/ru/netology/BasicAuto/BonusService/BonusServiceTest.java
+++ src/test/java/ru/netology/BasicAuto/BonusService/BonusServiceTest.java
@@ -1,19 +1,31 @@
 package ru.netology.BasicAuto.BonusService;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 class BonusServiceTest {
 
+
     @Test
-    void calculateBonus() {
+    public void calculateBonus() {
         BonusService service = new BonusService();
         int expected = 10;
         int amount = 2000;
 
         int actual = service.calculateBonus(amount);
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
+
+    }
+    
+    @Test
+    public void by1000() {
+        BonusService service = new BonusService();
+        int expected = 1;
+        int amount = 1000;
+
+        int actual = service.calculateBonus(amount);
+
+        Assert.assertEquals(actual, expected);
     }
 }
